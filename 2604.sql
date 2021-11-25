9777b71289efeb7fd2b1106ec7fd67c1e88b37eb
Alex Sander <33581240+Alex2Felicio@users.noreply.github.com>
2021-11-25 08:46:04 -0300

    Create 2604.sql

---
+++ 2604.sql
@@ -0,0 +1,3 @@
+O setor financeiro da empresa precisa de um relatório que mostre o código e o nome dos produtos cujo preço são menores que 10 ou maiores que 100.
+
+SELECT id, name FROM products WHERE price > 100 OR price < 10
