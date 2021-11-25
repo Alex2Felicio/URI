5cdd1af4d2c9d1b9b4007b5615154f86c594cd93
Alex Sander <33581240+Alex2Felicio@users.noreply.github.com>
2021-11-25 09:14:29 -0300

    Delete 2606.sql

--- 2606.sql
+++
@@ -1,6 +0,0 @@
-Quando os dados foram migrados de Banco de Dados, houve um pequeno mal-entendido por parte do antigo DBA.
-
-Seu chefe precisa que você exiba o código e o nome dos produtos, cuja categoria inicie com 'super'.
-
-
-SELECT p.id, p.name FROM products AS p INNER JOIN categories AS c ON c.id = p.id_categories WHERE c.name LIKE 'super%'
